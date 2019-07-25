3d480913f6233dc9b83c182472f448e6cc041586
kgtkr <kgtkr.jp@gmail.com>
2019-07-25 17:44:56 +0900

    config

---
+++ src/config.ts
@@ -0,0 +1,33 @@
+import * as t from "io-ts";
+import lazyValue from "lazy-value";
+import yaml from "js-yaml";
+import * as fs from "fs-extra";
+import { isRight } from "fp-ts/lib/Either";
+
+const tokenType = t.strict({
+  ck: t.string,
+  cs: t.string,
+  tk: t.string,
+  ts: t.string
+});
+
+const configType = t.strict({
+  consumers: t.array(t.strict({ ck: t.string, cs: t.string })),
+  tokens: t.array(tokenType),
+  ff_monitoring: t.strict({
+    interval: t.number,
+    tokens: t.array(tokenType)
+  })
+});
+
+export const config = lazyValue(async () => {
+  const data: unknown = yaml.safeLoad(
+    await fs.readFile("config.yaml", { encoding: "utf8" })
+  );
+  const config = configType.decode(data);
+  if (isRight(config)) {
+    return config.right;
+  } else {
+    throw config.left;
+  }
+});
