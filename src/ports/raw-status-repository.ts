580ef421bee49ddb79cee732a3fd45ed03ddc998
kgtkr <kgtkr.jp@gmail.com>
2019-07-27 12:57:09 +0900

    単純化

--- src/ports/raw-status-repository.ts
+++
@@ -1,7 +0,0 @@
-import { RawStatus } from "../entities/raw-status";
-import { TaskEither } from "fp-ts/lib/TaskEither";
-
-export interface RawStatusRepository {
-  insert(status: RawStatus): TaskEither<null, null>;
-  findLatest(ids: string[]): TaskEither<null, RawStatus[]>;
-}
