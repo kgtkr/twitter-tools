2ab9cabf4d70489b9ba31fbe459beb5642ee7da9
kgtkr <kgtkr.jp@gmail.com>
2019-07-28 18:09:32 +0900

    UserCache

---
+++ src/adaptors/user-cache.ts
@@ -0,0 +1,43 @@
+import { Twitter } from "./twitter";
+import { RawRepository } from "./raw-repository";
+import { set, map } from "fp-ts";
+import { eqString } from "fp-ts/lib/Eq";
+
+export class UserCache {
+  constructor(
+    public readonly twitter: Twitter,
+    public readonly rawRepo: RawRepository
+  ) {}
+
+  async lookupUsers(
+    ids: Set<string>,
+    now: Date
+  ): Promise<Map<string, unknown>> {
+    const dbUsers = await this.rawRepo
+      .findLatest("user", Array.from(ids))
+      .then(x => new Map(x.map(x => [x.id, x.raw])));
+
+    const difference = set.difference(eqString);
+
+    const fetchedUserArr = await this.twitter.lookupUsers(
+      Array.from(difference(ids, new Set(dbUsers.keys())))
+    );
+
+    await Promise.all(
+      fetchedUserArr.map(async user =>
+        this.rawRepo.insert({
+          type: "user",
+          id: user.id_str,
+          createdAt: now,
+          raw: user
+        })
+      )
+    );
+
+    const fetchedUsers = new Map<string, unknown>(
+      fetchedUserArr.map(x => [x.id_str, x])
+    );
+
+    return new Map([...Array.from(dbUsers), ...Array.from(fetchedUsers)]);
+  }
+}
